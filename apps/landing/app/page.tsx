73b5b8d283a6c2224215dc1634da46df7d0d8357
Caue Sampaio <caue_sampaio@hotmail.com>
2023-12-01 15:47:07 -0300

    First commit

---
+++ apps/landing/app/page.tsx
@@ -0,0 +1,25 @@
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span>Boas vindas a</span>
+              eRetail Labs, especialistas no varejo online!
+              <span>
+                Atualmente nosso site esta em construção, por favor, volte novamente em
+                breve...
+              </span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/landing/jest.config.ts
+++ apps/landing/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'eretaillabs-frontend-monorepo',
+  displayName: 'landing',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/eretaillabs-frontend-monorepo',
+  coverageDirectory: '../../coverage/apps/landing',
 };
